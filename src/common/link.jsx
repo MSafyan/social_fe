f51b7a09e9e16f1b0df80abcde186767b1444ce4
MSafyan <msafyan080@gmail.com>
2022-10-15 15:37:02 +0500

    doot linked

--- src/common/link.jsx
+++ src/common/link.jsx
@@ -1,2 +1,3 @@
 export const nftLink = 'http://localhost:3000';
-export const dootLink = 'http://localhost:3000/auth-login';
+// export const dootLink = 'http://localhost:3000/auth-login';
+export const dootLink = 'https://dootfe.vercel.app/auth-login';
--- src/components/posts/post/components/media.jsx
+++ src/components/posts/post/components/media.jsx
@@ -1,23 +1,25 @@
 function Media({ post }) {
-    return (
-        <div className="img-wrapper" style={{
-            marginBottom: !post.description && '0',
-            display: !post.imageUrl && !post.videoUrl && 'none',
-        }}>
-            {post?.imageUrl ?
-                <img src={post.imageUrl}
-                    className="img-fluid lazyload"
-                    alt=".." /> :
-                post?.videoUrl ?
-                    <video
-                        src={post.videoUrl}
-                        controls
-                    // preload="none"
-                    />
-                    : null
-            }
-        </div>
-    )
+	return (
+		<div
+			className='img-wrapper'
+			style={{
+				marginBottom: !post.description && '0',
+				display: !post.imageUrl && !post.videoUrl && 'none',
+			}}
+		>
+			{/* <img src='http://localhost:4000/IMG-20211114-WA0253.jpg'></img> */}
+			{/* <img src='https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350'></img> */}
+			{post?.imageUrl ? (
+				<img src={post.imageUrl} className='img-fluid lazyload' alt='..' />
+			) : post?.videoUrl ? (
+				<video
+					src={post.videoUrl}
+					controls
+					// preload="none"
+				/>
+			) : null}
+		</div>
+	);
 }
 
-export default Media
+export default Media;
