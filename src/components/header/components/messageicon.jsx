104f09ee1905794d7a8ed07bbaa6b8ce553c426c
MSafyan <msafyan080@gmail.com>
2022-10-15 17:01:08 +0500

    message icon

--- src/components/header/components/messageicon.jsx
+++ src/components/header/components/messageicon.jsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 import FeatherIcon from 'feather-icons-react';
+import { dootLink } from '../../../common/link';
+import { getJwt } from '../../../api/apiCore';
 
 function MessageIcon() {
-    return (
-        <>
-            <li className="header-btn custom-dropdown dropdown-lg btn-group message-btn">
-                <Link className="main-link" to="#">
-                    <FeatherIcon icon="message-circle" size={16} className="stroke-width-3 icon-light" />
-                </Link>
-            </li>
-        </>
-    )
+	return (
+		<>
+			<li className='header-btn custom-dropdown dropdown-lg btn-group message-btn'>
+				<Link className='main-link'>
+					<a onClick={() => window.open(`${dootLink}/${getJwt()}`, '_blank')}>
+						<FeatherIcon
+							icon='message-circle'
+							size={16}
+							className='stroke-width-3 icon-light'
+						/>
+					</a>
+				</Link>
+			</li>
+		</>
+	);
 }
 
-export default MessageIcon
+export default MessageIcon;
