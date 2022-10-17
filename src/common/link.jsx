4e2caa8ce6804d37e1fdbc376bcdf5d63f784044
MSafyan <msafyan080@gmail.com>
2022-10-17 10:42:45 +0500

    linked-1

--- src/common/link.jsx
+++ src/common/link.jsx
@@ -1,3 +1,3 @@
 export const nftLink = 'https://nft-marketplace-msafyan.vercel.app';
-// export const nftLink = 'http://localhost:3000';
+// export const nftLink = 'http://localhost:3001';
 export const dootLink = 'https://dootfe.vercel.app/auth-login';
